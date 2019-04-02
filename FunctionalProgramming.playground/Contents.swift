9befaddbf6b0f8c6aa0e00a4b0a2a07d0c7aa573
David Dinh <dmdinh22@gmail.com>
2019-04-02 01:48:33 -0700

    amusement park ride model

--- FunctionalProgramming.playground/Contents.swift
+++ FunctionalProgramming.playground/Contents.swift
@@ -13,3 +13,47 @@ func superHero() {
 print("original state = \(thing)")
 superHero()
 print("mutated state = \(thing)")
+
+// Model Amusement Park
+enum RideCategory: String, CustomStringConvertible {
+    case family
+    case kids
+    case thrill
+    case scary
+    case relaxing
+    case water
+    
+    var description: String {
+        return rawValue
+    }
+}
+
+typealias Minutes = Double
+
+struct Ride: CustomStringConvertible {
+    let name: String
+    let categories: Set<RideCategory>
+    let waitTime: Minutes
+    
+    var description: String {
+        return "Ride –\"\(name)\", wait: \(waitTime) mins, " +
+        "categories: \(categories)\n"
+    }
+}
+
+let parkRides = [
+    Ride(name: "Raging Rapids",
+         categories: [.family, .thrill, .water],
+         waitTime: 45.0),
+    Ride(name: "Crazy Funhouse", categories: [.family], waitTime: 10.0),
+    Ride(name: "Spinning Tea Cups", categories: [.kids], waitTime: 15.0),
+    Ride(name: "Spooky Hollow", categories: [.scary], waitTime: 30.0),
+    Ride(name: "Thunder Coaster",
+         categories: [.family, .thrill],
+         waitTime: 60.0),
+    Ride(name: "Grand Carousel", categories: [.family, .kids], waitTime: 15.0),
+    Ride(name: "Bumper Boats", categories: [.family, .water], waitTime: 25.0),
+    Ride(name: "Mountain Railroad",
+         categories: [.family, .relaxing],
+         waitTime: 0.0)
+]
